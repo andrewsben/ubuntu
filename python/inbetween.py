96e34365d59f0d35ef953314e0d4cdf4a862533a
Ben Andrews <andrewsben@gmail.com>
2013-11-11 12:18:59 -0500

    showing chad something

--- python/inbetween.py
+++ python/inbetween.py
@@ -19,53 +19,48 @@ starting_deck = [
 
 pot = 1000
 bankroll = 1000
-
+deck = starting_deck[:]
 
 while pot > 0 and bankroll > 0:
-	deck = starting_deck[:]
+	if len(deck) < 3:
+		deck = starting_deck[:]
 	print "shuffeling the deck!"
 	random.shuffle(deck)
+	left = deck.pop()
+	right = deck.pop()
 
-	while len(deck) >= 3:
-		left = deck.pop()
-		right = deck.pop()
+	print "\n\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+	print "Pot = $%d \tBankroll = $%d \nLeft: %s \tRight: %s\n" % (pot, bankroll, left, right)
+	print "--------------------------------------------------"
 
-		print "\n\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-		print "Pot = $%d \tBankroll = $%d \nLeft: %s \tRight: %s\n" % (pot, bankroll, left, right)
-
+	while True:
+		try:
+			bet = int(raw_input("Type the amount you want to bet (enter 0 to PASS): "))
+			break
+		except ValueError:
+			print "Oops, that's not a valid bet. Try again..."
 		print "--------------------------------------------------"
-
-		while True:
-			try:
-				bet = int(raw_input("Type the amount you want to bet (enter 0 to PASS): "))
-				break
-			except ValueError:
-				print "Oops, that's not a valid bet. Try again..."
-
-		print "--------------------------------------------------"
-
 		pot = pot + bet
-		bankroll = bankroll - bet
 
-		print "The pot is $%d and your bankroll is: $%d" % (pot, bankroll)
+	bankroll = bankroll - bet
+	print "The pot is $%d and your bankroll is: $%d" % (pot, bankroll)
 
-		if bet == 0:
-			print "PASSED - thanks for playing!"
-			print "--------------------------------------------------"
+	if bet == 0:
+		print "PASSED - thanks for playing!"
+		print "--------------------------------------------------"
+	else:
+		middle = deck.pop()
+		print "Your card: %s" % (middle)
+		if (middle < left and middle > right) or (middle > left and middle < right):
+			pot = pot - ( bet * 2 )
+			bankroll = bankroll + ( bet * 2 )
+			print "----------\n|        |\n|  WIN!  |\n|        |\n----------"
+			print "Bankroll = $%d" % bankroll
+			print "Thanks for playing!"
+			print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\n"
 		else:
-			middle = deck.pop()
-			print "Your card: %s" % (middle)
-
-			if (middle < left and middle > right) or (middle > left and middle < right):
-				pot = pot - ( bet * 2 )
-				bankroll = bankroll + ( bet * 2 )
-				print "----------\n|        |\n|  WIN!  |\n|        |\n----------"
-				print "Bankroll = $%d" % bankroll
-				print "Thanks for playing!"
-				print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\n"
-			else:
-				print "\t\t----------\n\t\t|        |\n\t\t|  LOST  |\n\t\t|        |\n\t\t----------"
-				print "Bankroll = $%d" % bankroll
-				print "Thanks for playing!"
-				print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\n"
+			print "\t\t----------\n\t\t|        |\n\t\t|  LOST  |\n\t\t|        |\n\t\t----------"
+			print "Bankroll = $%d" % bankroll
+			print "Thanks for playing!"
+			print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\n"
 
